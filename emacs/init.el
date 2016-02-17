d8499e718853152699e93a3332b0cf1a7a6b74c7
Mike Clarke <clarkema@clarkema.org>
2016-02-17 23:00:13 +0000

    Initial commit of emacs config

---
+++ emacs/init.el
@@ -0,0 +1,63 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(defvar my-packages '(company
+                       clojure-mode
+                       cider
+                       solarized-theme
+                       win-switch))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(add-to-list 'load-path "~/.emacs.d/packages")
+
+(require 'julia-mode)
+
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(when window-system (set-exec-path-from-shell-PATH))
+
+(require 'ido)
+(ido-mode t)
+
+(require 'fill-column-indicator)
+
+;(load (expand-file-name "~/quicklisp/slime-helper.el"))
+(add-to-list 'load-path "~/.emacs.d/vendor/sly")
+(require 'sly-autoloads)
+;; Replace "sbcl" with the path to your implementation
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+;(setq inferior-lisp-program "/Users/clarkema/.nomad/bin/lw-console")
+(setq sly-contribs '(sly-fancy))
+(add-hook 'lisp-mode-hook #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook #'show-paren-mode)
+(add-hook 'lisp-mode-hook #'fci-mode)
+(add-hook 'lisp-mode-hook #'paredit-mode)
+
+;; Don't treat the right-hand alt key on a Mac as Meta; this leaves it
+;; free for use in key combinations such as # (alt-3) and € (alt-2).
+(setq ns-right-alternate-modifier 'none)
+
+(load-theme 'solarized-light t)
+(set-cursor-color "#ff0000")
+(require 'column-marker)
+(require 'flymake)
+(setq ispell-program-name "aspell")
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-buffer)
+
+(setq visible-bell nil)
+
+(global-set-key "\C-xo" 'win-switch-dispatch)
+
+(add-hook 'after-init-hook 'global-company-mode)
+
+(setq next-line-add-newlines t)
+
