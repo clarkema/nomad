82ebad6729972455f6b6fbf9bd1f00004f0f133f
Mike Clarke <clarkema@clarkema.org>
2018-09-04 20:21:18 +0100

    [hs] Add three pane configuration for ultrawides

--- dotfiles/.hammerspoon/init.lua
+++ dotfiles/.hammerspoon/init.lua
@@ -7,6 +7,57 @@ end)
 hs.hotkey.bind(mash, ".", hs.hints.windowHints)
 hs.window.animationDuration = 0
 
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "7", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "8", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 4)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "9", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 3)
+  f.y = max.y
+  f.w = max.w / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "0", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + ((max.w / 3) * 2)
+  f.y = max.y
+  f.w = max.w / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
 
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
   local win = hs.window.focusedWindow()
@@ -146,47 +197,47 @@ renderStatuslets()
 --
 -- See https://github.com/tekezo/Karabiner-Elements/issues/8
 
-ctrl_table = {
-    sends_escape = true,
-    last_mods = {}
-}
-
-control_key_timer = hs.timer.delayed.new(0.15, function()
-    ctrl_table["send_escape"] = false
-    -- log.i("timer fired")
-    -- control_key_timer:stop()
-end
-)
-
-last_mods = {}
-
-control_handler = function(evt)
-  local new_mods = evt:getFlags()
-  if last_mods["ctrl"] == new_mods["ctrl"] then
-      return false
-  end
-  if not last_mods["ctrl"] then
-    -- log.i("control pressed")
-    last_mods = new_mods
-    ctrl_table["send_escape"] = true
-    -- log.i("starting timer")
-    control_key_timer:start()
-  else
-    -- log.i("contrtol released")
-    -- log.i(ctrl_table["send_escape"])
-    if ctrl_table["send_escape"] then
-      -- log.i("send escape key...")
-      hs.eventtap.keyStroke({}, "ESCAPE")
-    end
-    last_mods = new_mods
-    control_key_timer:stop()
-  end
-  return false
-end
-
-control_tap = hs.eventtap.new({12}, control_handler)
-
-control_tap:start()
+-- ctrl_table = {
+--     sends_escape = true,
+--     last_mods = {}
+-- }
+-- 
+-- control_key_timer = hs.timer.delayed.new(0.1, function()
+--     ctrl_table["send_escape"] = false
+--     -- log.i("timer fired")
+--     -- control_key_timer:stop()
+-- end
+-- )
+-- 
+-- last_mods = {}
+-- 
+-- control_handler = function(evt)
+--   local new_mods = evt:getFlags()
+--   if last_mods["ctrl"] == new_mods["ctrl"] then
+--       return false
+--   end
+--   if not last_mods["ctrl"] then
+--     -- log.i("control pressed")
+--     last_mods = new_mods
+--     ctrl_table["send_escape"] = true
+--     -- log.i("starting timer")
+--     control_key_timer:start()
+--   else
+--     -- log.i("contrtol released")
+--     -- log.i(ctrl_table["send_escape"])
+--     if ctrl_table["send_escape"] then
+--       -- log.i("send escape key...")
+--       hs.eventtap.keyStroke({}, "ESCAPE")
+--     end
+--     last_mods = new_mods
+--     control_key_timer:stop()
+--   end
+--   return false
+-- end
+-- 
+-- control_tap = hs.eventtap.new({12}, control_handler)
+-- 
+-- control_tap:start()
 
 --
 -- End Ctrl/Esc hack
