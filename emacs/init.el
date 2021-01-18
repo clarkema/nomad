9f252a0abb9de4792e5e8b952caadf7a77985b3a
Mike Clarke <clarkema@clarkema.org>
2021-01-18 21:35:25 +0000

    [emacs] Remove some more Custom junk

--- emacs/init.el
+++ emacs/init.el
@@ -245,17 +245,6 @@
 
 (load "nomad-lisp")
 (load "nomad-clojure")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" default)))
- '(package-selected-packages
-   (quote
-    (win-switch w3m solarized-theme sly restclient-helm popwin monokai-theme markdown-mode+ magit-popup magit macrostep helm-rg helm-cider ghub fill-column-indicator company color-theme-monokai color-theme-molokai clojure-mode-extra-font-locking clj-refactor ace-window))))
 (use-package notmuch
   :defer t
   :bind (:map notmuch-show-mode-map
