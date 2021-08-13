5588b451ac7dbdfc3a4dc66d329c9fa78fcb57c4
Mike Clarke <clarkema@clarkema.org>
2021-08-13 12:27:53 +0100

    [emacs] Tweak sly settings for macOS

--- emacs/language-config/nomad-lisp.el
+++ emacs/language-config/nomad-lisp.el
@@ -8,16 +8,21 @@
 ;(add-hook 'sly-mode-hook 'sly-company-mode)
 ;(eval-after-load 'company
   ;'(add-to-list
-    ;'company-backends '(sly-company)))
+                                        ;'company-backends '(sly-company)))
+
+(use-package sly
+  :defer t
+  :straight t)
+
 ;; Replace "sbcl" with the path to your implementation
-(setq inferior-lisp-program "/usr/bin/sbcl")
+(setq inferior-lisp-program
+      (if (string-equal system-type "darwin")
+          "/opt/homebrew/bin/sbcl"
+        "/usr/bin/sbcl"))
+
 ;(setq inferior-lisp-program "/Users/clarkema/.nomad/bin/lw-console")
 ;(setq sly-contribs '(sly-fancy))
 (add-hook 'lisp-mode-hook #'enable-paredit-mode)
 (add-hook 'lisp-mode-hook #'show-paren-mode)
 (add-hook 'lisp-mode-hook #'fci-mode)
 (add-hook 'lisp-mode-hook #'paredit-mode)
-
-;; (use-package slime
-;;   :defer t
-;;   :straight t)
