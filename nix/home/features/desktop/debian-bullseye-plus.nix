a7ff6361fef04b97b1d4b6d1b4efaf28189ade8a
Mike Clarke <clarkema@clarkema.org>
2023-03-26 14:27:56 +0100

    [nix] Add ungoogled-chromium to bullseye-plus

--- nix/home/features/desktop/debian-bullseye-plus.nix
+++ nix/home/features/desktop/debian-bullseye-plus.nix
@@ -1,13 +1,14 @@
 { pkgs, ... }:
 
+let binDir = "/home/clarkema/.nix-profile/bin"; in
 {
   home.packages = [
     pkgs.kitty
     pkgs.nixgl.nixGLIntel
+    pkgs.ungoogled-chromium
   ];
 
   home.file.".local/share/applications/kitty.desktop".text =
-    let binDir = "/home/clarkema/.nix-profile/bin"; in
     ''
     [Desktop Entry]
     Version=1.0
@@ -17,5 +18,25 @@
     Comment=Fast, feature-rich, GPU based terminal
     Exec=${binDir}/nixGLIntel ${binDir}/kitty
     Categories=System;TerminalEmulator;
+    '';
+
+  home.file.".local/share/applications/chromium.desktop".text =
+    ''
+    [Desktop Entry]
+    StartupWMClass=chromium-browser
+    Version=1.0
+    Name=Chromium
+    # Only KDE 4 seems to use GenericName, so we reuse the KDE strings.
+    # From Ubuntu's language-pack-kde-XX-base packages, version 9.04-20090413.
+    GenericName=Web Browser
+    Comment=Access the Internet
+    Exec=${binDir}/chromium %U
+    StartupNotify=true
+    Terminal=false
+    Icon=chromium
+    Type=Application
+    Categories=Network;WebBrowser;
+    MimeType=application/pdf;application/rdf+xml;application/rss+xml;application/xhtml+xml;application/xhtml_xml;application/xml;image/gif;image/jpeg;image/png;image/webp;text/html;text/xml;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/webcal;x-scheme-handler/mailto;x-scheme-handler/about;x-scheme-handler/unknown
+    Actions=new-window;new-private-window;
     '';
 }
