2055f8b1658f7542e259c11480e4debe72fce309
Mike Clarke <clarkema@clarkema.org>
2023-11-29 00:00:04 +0000

    [nix/munnin] Add sway configuration

--- nix/home/system/munnin.nix
+++ nix/home/system/munnin.nix
@@ -2,5 +2,25 @@
   imports = [ ../all.nix ];
 
   home.packages = with pkgs; [
+    kitty
+    aspell
+    aspellDicts.en
+    aspellDicts.en-science
+    aspellDicts.en-computers
+    wdisplays # Wayland display manager
   ];
+
+  wayland.windowManager.sway = {
+    enable = true;
+    config = rec {
+      modifier = "Mod4";
+      terminal = "kitty";
+
+      input = {
+        "*" = {
+	  "xkb_layout" = "gb";
+	};
+      };
+    };
+  };
 }
