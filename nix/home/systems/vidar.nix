7162b3f41466e64477e086404f337ad39c0c2e8c
Mike Clarke <clarkema@clarkema.org>
2023-12-11 22:19:08 +0000

    [nix/vidar] Update packages

--- nix/home/systems/vidar.nix
+++ nix/home/systems/vidar.nix
@@ -1,7 +1,23 @@
 { inputs, pkgs, ... }: {
   imports = [ ../all.nix ];
 
-  home.packages = with pkgs; [
+  home.packages =
+  let
+    rakudo_env = pkgs.buildEnv {
+      name = "rakudo-env";
+      paths = [
+        pkgs.readline
+        pkgs.rakudo
+        pkgs.zef
+        pkgs.util-linux
+        pkgs.zlib
+      ];
+      pathsToLink = [ "/bin" "/lib" "/share" ];
+    };
+  in with pkgs; [
     signal-desktop
+    rakudo_env
   ];
+
+  home.file.".local/share/applications/signal-desktop.desktop".source = "${pkgs.signal-desktop}/share/applications/signal-desktop.desktop";
 }
