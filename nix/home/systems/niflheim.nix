cecc63e6fa270ef965e385b471ecc50d4538f890
Mike Clarke <clarkema@clarkema.org>
2023-12-19 16:32:32 +0000

    [nix/niflheim] Add chromium desktop link

--- nix/home/systems/niflheim.nix
+++ nix/home/systems/niflheim.nix
@@ -21,4 +21,6 @@ let binDir = "/home/clarkema/.nix-profile/bin"; in
   ];
 
   home.file.".local/share/applications/mullvad-browser.desktop".source = "${pkgs.mullvad-browser}/share/applications/mullvadbrowser.desktop";
+
+  home.file.".local/share/applications/chromium.desktop".source = "${pkgs.ungoogled-chromium}/share/applications/chromium-browser.desktop";
 }
