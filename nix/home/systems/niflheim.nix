298c053ead7106d5577b2591b177e3255f3a807b
Mike Clarke <clarkema@clarkema.org>
2024-04-28 18:34:01 +0100

    [nix/niflheim] Fix mullvad-browser desktop file

--- nix/home/systems/niflheim.nix
+++ nix/home/systems/niflheim.nix
@@ -32,11 +32,32 @@ in
   ]
   ++ myPkgs;
 
-  home.file.".local/share/applications/mullvad-browser.desktop".source = "${pkgs.mullvad-browser}/share/applications/mullvad-browser.desktop";
-
   home.file.".local/share/applications/chromium.desktop".source = "${pkgs.ungoogled-chromium}/share/applications/chromium-browser.desktop";
 
   home.file.".local/share/applications/1password.desktop".source = "${pkgs._1password-gui}/share/applications/1password.desktop";
 
   home.file.".local/share/applications/signal-desktop.desktop".source = "${pkgs.signal-desktop}/share/applications/signal-desktop.desktop";
+
+  home.file.".local/share/applications/mullvad-browser.desktop" = {
+    text =
+      ''
+      [Desktop Entry]
+      StartupWMClass=mullvad-browser
+      Version=1.0
+      Name=Mullvad Browser
+      # Only KDE 4 seems to use GenericName, so we reuse the KDE strings.
+      # From Ubuntu's language-pack-kde-XX-base packages, version 9.04-20090413.
+      GenericName=Web Browser
+      Comment=Access the Internet
+      Exec=${binDir}/mullvad-browser %U
+      StartupNotify=true
+      Terminal=false
+      Icon=mullvad-browser
+      Type=Application
+      Categories=Network;WebBrowser;
+      MimeType=application/pdf;application/rdf+xml;application/rss+xml;application/xhtml+xml;application/xhtml_xml;application/xml;image/gif;image/jpeg;image/png;image/webp;text/html;text/xml;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/webcal;x-scheme-handler/mailto;x-scheme-handler/about;x-scheme-handler/unknown
+      Actions=new-window;new-private-window;
+      '';
+    executable = true;
+  };
 }
