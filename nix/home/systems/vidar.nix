8e679c6192f4a8c2e367b17e97049349f33e5c82
Mike Clarke <clarkema@clarkema.org>
2024-07-04 12:00:15 +0100

    [nix/vidar] Add desktop entry for Chirp

--- nix/home/systems/vidar.nix
+++ nix/home/systems/vidar.nix
@@ -1,4 +1,7 @@
-{ inputs, pkgs, ... }: {
+{ inputs, pkgs, ... }:
+
+let binDir = "/home/clarkema/.nix-profile/bin"; in
+{
   imports = [ ../all.nix ];
 
   home.packages =
@@ -24,4 +27,22 @@
   ];
 
   home.file.".local/share/applications/signal-desktop.desktop".source = "${pkgs.signal-desktop}/share/applications/signal-desktop.desktop";
+
+  home.file.".local/share/applications/chirp.desktop".text =
+    ''
+    [Desktop Entry]
+    Type=Application
+    Version=1.0
+    Name=CHIRP
+    GenericName=Radio Programming Tool
+    Comment=Program amateur radios
+    Icon=${pkgs.chirp}/lib/python3.11/site-packages/chirp/share/chirp
+    Exec=${binDir}/chirp %F
+    Terminal=false
+    MimeType=x-scheme-handler/chirp
+    Categories=Utility;HamRadio
+    Keywords=Hamradio;Programming;Handheld;Radio;Amateur;Programmer
+    StartupNotify=true
+    }
+    '';
 }
