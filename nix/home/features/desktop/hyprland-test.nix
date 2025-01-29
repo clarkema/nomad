79c66dd705fe9c94e8c40946b39e4dbe16397a94
Mike Clarke <clarkema@clarkema.org>
2025-01-29 14:16:11 +0000

    [nix] Update hyprland config
    Further testing

--- nix/home/features/desktop/hyprland-test.nix
+++ nix/home/features/desktop/hyprland-test.nix
@@ -1,9 +1,12 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 
-let binDir = "/home/clarkema/.nix-profile/bin"; in
+let
+  hyprpkgs = import inputs.hyprland {
+    system = "x86_64-linux";
+  };
+in
 {
   home.packages = [
-    pkgs.hyprland
     pkgs.wofi
     pkgs.light
   ];
@@ -14,5 +17,50 @@ let binDir = "/home/clarkema/.nix-profile/bin"; in
       # Required to be able to use the wlr/workspaces module
       mesonFlags = (oa.mesonFlags or []) ++ [ "-Dexperimental=true" ];
     });
+  };
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    settings = {
+      "$mod" = "SUPER";
+      bind = [
+        "$mod, F, exec, /home/clarkema/.nix-profile/bin/kitty"
+        "$mod, B, exec, firefox"
+
+      ];
+    };
+    extraConfig = ''
+      $menu = wofi --show drun
+      input {
+        kb_layout = gb
+      }
+
+      animations {
+        enabled = false
+      }
+
+      decoration {
+        blur {
+          enabled = false
+        }
+        shadow {
+          enabled = false
+        }
+      }
+
+      misc {
+        vfr = true
+        disable_hyprland_logo = true
+      }
+
+      bind = $mod, W, killactive
+      bind = $mod, V, togglefloating
+      bind = $mod, space, exec, $menu
+
+      # Move / resize windows with mod+LMB/RMB
+      bindm = $mod, mouse:272, movewindow
+      bindm = $mod, mouse:273, resizewindow
+    '';
   };
 }
