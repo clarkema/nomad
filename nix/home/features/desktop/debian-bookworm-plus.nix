1119fa9c124f4458e4cf3ec8de65ccc5aa42f895
Mike Clarke <clarkema@clarkema.org>
2025-03-03 10:48:47 +0000

    [nix/bookworm] Add Librewolf

--- nix/home/features/desktop/debian-bookworm-plus.nix
+++ nix/home/features/desktop/debian-bookworm-plus.nix
@@ -6,8 +6,10 @@ let binDir = "/home/clarkema/.nix-profile/bin"; in
     ./chromium.nix
   ];
 
-  home.packages = [
-    pkgs.mullvad-browser
+  home.packages = with pkgs; [
+    nixgl.nixGLIntel
+    mullvad-browser
+    librewolf
   ];
 
   home.file.".local/share/applications/mullvad-browser.desktop".text =
@@ -24,6 +26,26 @@ let binDir = "/home/clarkema/.nix-profile/bin"; in
     StartupNotify=true
     Terminal=false
     Icon=mullvad-browser
+    Type=Application
+    Categories=Network;WebBrowser;
+    MimeType=application/pdf;application/rdf+xml;application/rss+xml;application/xhtml+xml;application/xhtml_xml;application/xml;image/gif;image/jpeg;image/png;image/webp;text/html;text/xml;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/webcal;x-scheme-handler/mailto;x-scheme-handler/about;x-scheme-handler/unknown
+    Actions=new-window;new-private-window;
+    '';
+
+  home.file.".local/share/applications/librewolf.desktop".text =
+    ''
+    [Desktop Entry]
+    StartupWMClass=librewolf
+    Version=1.4
+    Name=Librewolf (Nix)
+    # Only KDE 4 seems to use GenericName, so we reuse the KDE strings.
+    # From Ubuntu's language-pack-kde-XX-base packages, version 9.04-20090413.
+    GenericName=Web Browser
+    Comment=Access the Internet
+    Exec=${pkgs.nixgl.nixGLIntel}/bin/nixGLIntel ${binDir}/librewolf %U
+    StartupNotify=true
+    Terminal=false
+    Icon=librewolf
     Type=Application
     Categories=Network;WebBrowser;
     MimeType=application/pdf;application/rdf+xml;application/rss+xml;application/xhtml+xml;application/xhtml_xml;application/xml;image/gif;image/jpeg;image/png;image/webp;text/html;text/xml;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/webcal;x-scheme-handler/mailto;x-scheme-handler/about;x-scheme-handler/unknown
