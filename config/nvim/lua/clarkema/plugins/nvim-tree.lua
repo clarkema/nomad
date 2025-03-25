12691fd7be33d1b7291b3eef0bb94d7310a38343
Mike Clarke <clarkema@clarkema.org>
2025-03-25 19:17:54 +0000

    [nvim] Tweak nvim-tree bindings

--- config/nvim/lua/clarkema/plugins/nvim-tree.lua
+++ config/nvim/lua/clarkema/plugins/nvim-tree.lua
@@ -37,7 +37,8 @@ return {
         local keymap = vim.keymap
 
         keymap.set("n", "<leader>ee", "<cmd>NvimTreeFocus<CR>", { desc = "Toggle file explorer" })
-        keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFile<CR>", { desc = "Toggle file explorer on current file" })
+        keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFile<CR>", { desc = "Open file explorer on current file" })
+        keymap.set("n", "<leader>ex", "<cmd>NvimTreeClose<CR>", { desc = "Close file explorer" })
         keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
         keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
     end
