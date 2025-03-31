3e13711ba053c21bae32f118ed4eaf9acd42ebc7
Mike Clarke <clarkema@clarkema.org>
2025-03-31 22:44:48 +0100

    [nvim] Paste default keybinds for trouble.nvim

--- config/nvim/lua/clarkema/plugins/trouble.lua
+++ config/nvim/lua/clarkema/plugins/trouble.lua
@@ -4,10 +4,11 @@ return {
     opts = {},
     cmd = "Trouble",
     keys = {
-        { "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", desc = "Toggle trouble list" },
-        { "<leader>xw", "<cmd>TroubleToggle workspace_dianostics<CR>", desc = "Toggle trouble workspace diagnostics" },
-        { "<leader>xd", "<cmd>TroubleToggle document_dianostics<CR>", desc = "Toggle trouble document diagnostics" },
-        { "<leader>xq", "<cmd>Trouble qflist toggle<CR>", desc = "Open trouble quickfix list" },
-        { "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
+        { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)", },
+        { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)", },
+        { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)", },
+        { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions / references / ... (Trouble)", },
+        { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)", },
+        { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)", },
     },
 }
