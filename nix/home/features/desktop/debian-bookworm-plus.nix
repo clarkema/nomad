abbdc7333bf83821b1aa7145cc143e881dc3ef54
Mike Clarke <clarkema@clarkema.org>
2025-05-08 17:26:52 +0100

    [nix] Fix typo in bookworm-plus

--- nix/home/features/desktop/debian-bookworm-plus.nix
+++ nix/home/features/desktop/debian-bookworm-plus.nix
@@ -30,3 +30,4 @@ let binDir = "/home/clarkema/.nix-profile/bin"; in
     MimeType=application/pdf;application/rdf+xml;application/rss+xml;application/xhtml+xml;application/xhtml_xml;application/xml;image/gif;image/jpeg;image/png;image/webp;text/html;text/xml;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/webcal;x-scheme-handler/mailto;x-scheme-handler/about;x-scheme-handler/unknown
     Actions=new-window;new-private-window;
     '';
+}
