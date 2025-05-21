1f8b15e6c70764efefdeaf2edb763588ad48da99
Mike Clarke <clarkema@clarkema.org>
2025-05-21 09:45:18 +0100

    [nvim] Re-enable netrw for scp

--- config/nvim/lua/clarkema/plugins/nvim-tree.lua
+++ config/nvim/lua/clarkema/plugins/nvim-tree.lua
@@ -4,9 +4,11 @@ return {
     config = function()
         local nvimtree = require("nvim-tree")
 
-        -- recommended settings from nvim-tree documentation
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
+        -- nvim-tree recommends this configuration to disable
+        -- netrw.  However, netrw provides nvim's support for
+        -- editing files via scp, which I use.
+        --vim.g.loaded_netrw = 1
+        --vim.g.loaded_netrwPlugin = 1
 
         nvimtree.setup({
             view = {
