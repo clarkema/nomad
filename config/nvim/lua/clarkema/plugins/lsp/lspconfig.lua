9120ce40b52fd6e66f3c8a73a4a75f4152ea439c
Mike Clarke <clarkema@clarkema.org>
2026-01-02 21:49:09 +0000

    [nvim] Update lspconfig for nvim 0.11

--- config/nvim/lua/clarkema/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/clarkema/plugins/lsp/lspconfig.lua
@@ -4,49 +4,27 @@ return {
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         { "antosha417/nvim-lsp-file-operations", config = true },
-        { "folke/neodev.nvim", opts = {} },
     },
     config = function()
-        -- import lspconfig plugin
-        local lspconfig = require("lspconfig")
-
-        -- import cmp_nvim_lsp plugin for integration with completion
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-        local keymap = vim.keymap
-
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-            callback = function(ev)
-                local opts = { buffer = ev.buf, silent = true }
-
-                opts.desc = "Show LSP references"
-                keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
-
-                opts.desc = "Go to declaration"
-                keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-
-                opts.desc = "Go to declaration"
-                keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-            end
-        })
 
         local capabilities = cmp_nvim_lsp.default_capabilities()
 
-        lspconfig["elixirls"].setup {
+        vim.lsp.config("*", {
             capabilities = capabilities,
-            cmd = { "elixir-ls" }
-        }
+        })
 
-        lspconfig["emmet_ls"].setup {
-            capabilities = capabilities,
-            filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" }
-        }
+        vim.lsp.enable("elixirls")
+        vim.lsp.enable("lua_ls")
+        --lspconfig["emmet_ls"].setup {
+            --capabilities = capabilities,
+            --filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" }
+        --}
 
-        lspconfig["bashls"].setup {
-            capabilities = capabilities
-        }
+        --lspconfig["bashls"].setup {
+            --capabilities = capabilities
+        --}
 
-        lspconfig["perlnavigator"].setup { }
+        --lspconfig["perlnavigator"].setup { }
     end
 }
