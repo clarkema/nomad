c480872ce115573355bb2f614e1c568ed02e5f04
Mike Clarke <clarkema@clarkema.org>
2026-01-12 13:32:21 +0000

    [wezterm] Move toggle fullscreen to CTRL+ALT+F11

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -54,17 +54,28 @@ config.keys = {
         mods = 'CTRL|SHIFT|ALT',
         key = 'T',
         action = act.PromptInputLine {
-        description = 'Enter new name for tab',
-        action = wezterm.action_callback(function(window, pane, line)
-            -- line will be `nil` if they hit escape without entering anything
-            -- An empty string if they just hit enter
-            -- Or the actual line of text they wrote
-            if line then
-                window:active_tab():set_title(line)
-            end
-        end),
+            description = 'Enter new name for tab',
+            action = wezterm.action_callback(function(window, pane, line)
+                -- line will be `nil` if they hit escape without entering anything
+                -- An empty string if they just hit enter
+                -- Or the actual line of text they wrote
+                if line then
+                    window:active_tab():set_title(line)
+                end
+            end),
+        },
     },
-  },
+    {
+        -- Disable default fullscreen binding
+        key = 'Enter',
+        mods = 'ALT',
+        action = wezterm.action.DisableDefaultAssignment,
+    },
+    {
+        key = 'F11',
+        mods = 'SHIFT|CTRL',
+        action = act.ToggleFullScreen,
+    },
 }
 
 return config
