aa2bca4a2d260fb31408b3ff9a3ffe8758db7850
Mike Clarke <clarkema@clarkema.org>
2026-04-08 07:44:02 +0100

    [nvim] Port across tab options from vim

--- config/nvim/lua/clarkema/core/options.lua
+++ config/nvim/lua/clarkema/core/options.lua
@@ -1,6 +1,22 @@
+-- vim: set foldlevel=0 foldmethod=marker:
+
 vim.cmd("let g:netrw_liststyle = 3")
 
 local opt = vim.opt
+
+-- Indenting and tabs {{{1
+
+opt.autoindent = true
+opt.smartindent = true
+
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.expandtab = true
+opt.smarttab = true
+opt.shiftround = true
+
+-- }}}
+
 
 --opt.relativenumber = true
 --opt.number = true
