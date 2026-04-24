8109bd12878474ab04eca36d57a651689e6a698d
Mike Clarke <clarkema@clarkema.org>
2026-04-24 16:23:11 +0100

    [nvim] Add new 'gg' keybinding to save and quit

--- config/nvim/lua/clarkema/core/keymaps.lua
+++ config/nvim/lua/clarkema/core/keymaps.lua
@@ -5,8 +5,8 @@ local keymap = vim.keymap -- for conciseness
 keymap.set("i", "kj", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-
 keymap.set("n", "<M-j>", "<cmd>cnext<CR>", { desc = "Move to next item in quickfix list" })
 keymap.set("n", "<M-k>", "<cmd>cprev<CR>", { desc = "Move to previous item in quickfix list" })
+
+keymap.set("n", "<leader>gg", ":x<CR>", { desc = "Save and quit" })
 
